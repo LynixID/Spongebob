9a2397aa9acf54079a9c74f94b9a68e8a3136116
LynixID <gadinglalala121212@gmail.com>
2024-01-30 12:26:11 +0700

    Memperbaiki sedikit kesalahan alert JS

--- script.js
+++ script.js
@@ -1,33 +1,3 @@
-// function checkScreenWidth() {
-//     var screenWidth = window.innerWidth ;
-//     //|| document.documentElement.clientWidth || document.body.clientWidth;
-
-    
-//     if (screenWidth <= 1024) {
-//         const hamburger = document.querySelector('#hamburger');
-//         const nav = document.querySelector('#list');
-//         const navbar = document.querySelector('#navbar header')
-
-//         hamburger.addEventListener('click', function(){
-//             nav.classList.toggle('translate-x-full');
-//             nav.classList.toggle('bg-white');
-//             navbar.classList.toggle('bg-white');
-//         });
-
-//         document.addEventListener('click', function(){
-//             if(hamburger.checked == true){
-//                 if(!nav.contains(event.target) && !navbar.contains(event.target)){
-//                 nav.classList.toggle('translate-x-full');
-//                 nav.classList.toggle('bg-white');
-//                 hamburger.checked = false;
-//             };
-//             }
-            
-//         });
-//     }
-// }
-
-
 //Toggle Menu Navbar di mode Mobile
 const hamburger = document.querySelector('#hamburger');
 const nav = document.querySelector('#list');
@@ -51,12 +21,6 @@ document.addEventListener('click', function(e){
     
 });
 
-// Panggil fungsi saat halaman dimuat dan saat ukuran layar berubah
-window.onload = checkScreenWidth;
-window.onresize = checkScreenWidth;
-
-//Feather icon
-feather.replace();
 
 //Alert untuk fitur yang belum ada
 const message = document.querySelector('#navbarMessage');
