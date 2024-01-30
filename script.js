bf7d7dd4920d6f8cad238d008837a792571c901a
LynixID <gadinglalala121212@gmail.com>
2024-01-30 12:15:16 +0700

    mengganti tema navbar menu mobile

--- script.js
+++ script.js
@@ -1,32 +1,55 @@
-function checkScreenWidth() {
-    var screenWidth = window.innerWidth ;
-    //|| document.documentElement.clientWidth || document.body.clientWidth;
+// function checkScreenWidth() {
+//     var screenWidth = window.innerWidth ;
+//     //|| document.documentElement.clientWidth || document.body.clientWidth;
 
     
-    if (screenWidth <= 1024) {
-        const hamburger = document.querySelector('#hamburger');
-        const nav = document.querySelector('#list');
-        const navbar = document.querySelector('#navbar header')
+//     if (screenWidth <= 1024) {
+//         const hamburger = document.querySelector('#hamburger');
+//         const nav = document.querySelector('#list');
+//         const navbar = document.querySelector('#navbar header')
 
-        hamburger.addEventListener('click', function(){
-            nav.classList.toggle('translate-x-full');
-            navbar.classList.toggle('backdrop-blur-xl');
-            navbar.classList.toggle('bg-white');
-        });
+//         hamburger.addEventListener('click', function(){
+//             nav.classList.toggle('translate-x-full');
+//             nav.classList.toggle('bg-white');
+//             navbar.classList.toggle('bg-white');
+//         });
 
-        document.addEventListener('click', function(){
-            if(hamburger.checked == true){
-                if(!nav.contains(event.target) && !navbar.contains(event.target)){
-                nav.classList.toggle('translate-x-full');
-                navbar.classList.toggle('backdrop-blur-xl');
-                navbar.classList.toggle('bg-white');
-                hamburger.checked = false;
-            };
-            }
+//         document.addEventListener('click', function(){
+//             if(hamburger.checked == true){
+//                 if(!nav.contains(event.target) && !navbar.contains(event.target)){
+//                 nav.classList.toggle('translate-x-full');
+//                 nav.classList.toggle('bg-white');
+//                 hamburger.checked = false;
+//             };
+//             }
             
-        });
+//         });
+//     }
+// }
+
+
+//Toggle Menu Navbar di mode Mobile
+const hamburger = document.querySelector('#hamburger');
+const nav = document.querySelector('#list');
+const navbar = document.querySelector('#navbar header')
+
+hamburger.addEventListener('click', function(){
+    nav.classList.toggle('translate-x-full');
+    nav.classList.toggle('bg-white');
+    navbar.classList.toggle('bg-white');
+});
+
+document.addEventListener('click', function(e){
+    if(hamburger.checked == true){
+        if(!nav.contains(e.target) && !navbar.contains(e.target)){
+        nav.classList.add('translate-x-full');
+        nav.classList.remove('bg-white');
+        navbar.classList.remove('bg-white');
+        hamburger.checked = false;
+    };
     }
-}
+    
+});
 
 // Panggil fungsi saat halaman dimuat dan saat ukuran layar berubah
 window.onload = checkScreenWidth;
@@ -44,4 +67,7 @@ message.addEventListener('click', function(){
 });
 merchantButton.addEventListener('click', function(){
     alert('Fitur yang anda pilih belum tersedia :)');
-})
+});
+
+
+
